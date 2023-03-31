d6d91ff47f753d5de77489be9e47d83d615fda24
Daniel <acidistrict@gmail.com>
2023-03-31 10:31:53 +0100

    importing data from json

--- src/pages/[productDetails].jsx
+++ src/pages/[productDetails].jsx
@@ -13,21 +13,21 @@ import useFetch from "@/hooks/useFetch";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import classes from "../styles/product-details.module.css";
+import produts from "../../public/data.json";
 
 const productDetails = () => {
   const router = useRouter();
-  const data = useFetch();
+
   const [product, setProduct] = useState({});
 
   useEffect(() => {
     if (router.query.productDetails) {
-      let result = data.find((product) => {
+      let result = produts.find((product) => {
         return product.slug === router.query.productDetails;
       });
       setProduct(result);
     }
   }, [router.query.productDetails]);
-
   return (
     <>
       <Header className={classes["bg-black"]} />
@@ -54,5 +54,31 @@ const productDetails = () => {
     </>
   );
 };
+
+// export const getStaticPaths = async () => {
+//   const response = await fetch("http://localhost:3000/data.json");
+//   const data = await response.json();
+
+//   const products = await data.find({}, { _slug: 1 }).toArray();
+
+//   return {
+//     fallback: false,
+//     paths: products.map((product) => ({
+//       params: { productDetails: product._slug.toString() },
+//     })),
+//   };
+// };
+
+// export const getStaticProps = async (context) => {
+//   const currentSlug = await context.params.productDetails;
+
+//   return {
+//     props: {
+//       data: data,
+//       slug: currentSlug,
+//     },
+//     revalidate: 1,
+//   };
+// };
 
 export default productDetails;
