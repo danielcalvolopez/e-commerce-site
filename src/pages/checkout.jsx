a986a7c2f1e8d14ec4e07c7257b6140bdb47c846
Daniel <acidistrict@gmail.com>
2023-04-13 18:47:41 +0100

    add router to go to the previous route

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -5,15 +5,17 @@ import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
 import MainContent from "@/components/UI/MainContent";
 import classes from "../styles/checkout.module.css";
+import { useRouter } from "next/router";
 
 const checkout = () => {
+  const router = useRouter();
   return (
     <>
       <Header className={classes["header-bg-black"]} />
       <MainContent bgGray>
         {/* <OrderConfirmationModal /> */}
         <div className={classes.content}>
-          <div className={classes.back}>
+          <div onClick={() => router.back()} className={classes.back}>
             <p>Go Back</p>
           </div>
           <div className={classes["body"]}>
