2f2d1020c19b9b5fcfed4b9cbfd7eb8b7d910e58
Daniel <acidistrict@gmail.com>
2023-04-15 23:05:32 +0100

    format numbers

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -5,13 +5,15 @@ const CartItem = ({ children, price, name, image }) => {
   itemName.pop();
   const finalName = itemName.join(" ");
 
+  const priceFormat = price.toLocaleString("en-US");
+
   return (
     <div className={classes["cart-item"]}>
       <div className={classes["cart-item-info"]}>
         <img className={classes.img} src={image} />
         <div className={classes.text}>
           <h6>{finalName}</h6>
-          <p className={classes.price}>$ {price}</p>
+          <p className={classes.price}>$ {priceFormat}</p>
         </div>
       </div>
       {children}
